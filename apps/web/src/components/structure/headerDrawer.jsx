dfee1418fd733dc8741234fab50427eb80217bb0
Matt Karmazyn <matt@itskarma.wtf>
2022-09-03 09:33:26 -0400

    moving navbar to side drawer

---
+++ apps/web/src/components/structure/headerDrawer.jsx
@@ -0,0 +1,209 @@
+import * as React from "react";
+import { Link } from "react-router-dom";
+import { styled } from "@mui/material/styles";
+import MuiDrawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+
+import RocketLaunchIcon from "@mui/icons-material/RocketLaunch";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import ListAltIcon from "@mui/icons-material/ListAlt";
+import EqualizerIcon from "@mui/icons-material/Equalizer";
+import InsightsIcon from "@mui/icons-material/Insights";
+import PeopleAltIcon from "@mui/icons-material/PeopleAlt";
+import VpnKeyIcon from "@mui/icons-material/VpnKey";
+import MemoryIcon from "@mui/icons-material/Memory";
+import LogoutIcon from "@mui/icons-material/Logout";
+
+import useStore from "../utils/appStore";
+
+const drawerWidth = 240;
+
+const pages = [
+  { name: "Dashboard", icon: <DashboardIcon /> },
+  { name: "Catalog", icon: <ListAltIcon /> },
+  { name: "Logs", icon: <EqualizerIcon /> },
+  { name: "Analytics", icon: <InsightsIcon /> },
+];
+
+const adminPages = [
+  { name: "Users", icon: <PeopleAltIcon /> },
+  { name: "ApiKeys", icon: <VpnKeyIcon /> },
+  { name: "SSO", icon: <MemoryIcon /> },
+];
+
+const openedMixin = (theme) => ({
+  width: drawerWidth,
+  transition: theme.transitions.create("width", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.enteringScreen,
+  }),
+  overflowX: "hidden",
+});
+
+const closedMixin = (theme) => ({
+  transition: theme.transitions.create("width", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  overflowX: "hidden",
+  width: `calc(${theme.spacing(7)} + 1px)`,
+  [theme.breakpoints.up("sm")]: {
+    width: `calc(${theme.spacing(8)} + 1px)`,
+  },
+});
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "flex-end",
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+}));
+
+const Drawer = styled(MuiDrawer, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  width: drawerWidth,
+  flexShrink: 0,
+  whiteSpace: "nowrap",
+  boxSizing: "border-box",
+  ...(open && {
+    ...openedMixin(theme),
+    "& .MuiDrawer-paper": openedMixin(theme),
+  }),
+  ...(!open && {
+    ...closedMixin(theme),
+    "& .MuiDrawer-paper": closedMixin(theme),
+  }),
+}));
+
+const MiniDrawer = () => {
+  const userRole = useStore((state) => state.role);
+  const open = useStore((state) => state.navOpen);
+  const setOpen = useStore((state) => state.setNavOpen);
+
+  const handleDrawerToggle = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <Drawer variant="permanent" open={open}>
+      <DrawerHeader>
+        {open ? (
+          <Typography variant="h6" sx={{ mr: "1rem" }}>
+            DeployBoard
+          </Typography>
+        ) : null}
+        <IconButton onClick={handleDrawerToggle}>
+          <RocketLaunchIcon sx={{ transform: open ? "rotate(-90deg)" : "" }} />
+        </IconButton>
+      </DrawerHeader>
+      <Divider />
+      <List>
+        {pages.map((item) => (
+          <ListItem key={item.name} disablePadding sx={{ display: "block" }}>
+            <ListItemButton
+              component={Link}
+              to={`/${item.name.toLowerCase()}`}
+              sx={{
+                minHeight: 48,
+                justifyContent: open ? "initial" : "center",
+                px: 2.5,
+              }}
+            >
+              <ListItemIcon
+                sx={{
+                  minWidth: 0,
+                  mr: open ? 3 : "auto",
+                  justifyContent: "center",
+                }}
+              >
+                {item.icon}
+              </ListItemIcon>
+              <ListItemText
+                primary={item.name}
+                sx={{ opacity: open ? 1 : 0 }}
+              />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+
+      {userRole === "Admin" ? (
+        <>
+          <Divider />
+          <List>
+            {adminPages.map((item) => (
+              <ListItem
+                key={item.name}
+                disablePadding
+                sx={{ display: "block" }}
+              >
+                <ListItemButton
+                  component={Link}
+                  to={`/account/${item.name.toLowerCase()}`}
+                  sx={{
+                    minHeight: 48,
+                    justifyContent: open ? "initial" : "center",
+                    px: 2.5,
+                  }}
+                >
+                  <ListItemIcon
+                    sx={{
+                      minWidth: 0,
+                      mr: open ? 3 : "auto",
+                      justifyContent: "center",
+                    }}
+                  >
+                    {item.icon}
+                  </ListItemIcon>
+                  <ListItemText
+                    primary={item.name}
+                    sx={{ opacity: open ? 1 : 0 }}
+                  />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+        </>
+      ) : null}
+
+      <Divider />
+
+      <List>
+        <ListItem disablePadding sx={{ display: "block" }}>
+          <ListItemButton
+            component={Link}
+            to="/logout"
+            sx={{
+              minHeight: 48,
+              justifyContent: open ? "initial" : "center",
+              px: 2.5,
+            }}
+          >
+            <ListItemIcon
+              sx={{
+                minWidth: 0,
+                mr: open ? 3 : "auto",
+                justifyContent: "center",
+              }}
+            >
+              <LogoutIcon />
+            </ListItemIcon>
+            <ListItemText primary="Log Out" sx={{ opacity: open ? 1 : 0 }} />
+          </ListItemButton>
+        </ListItem>
+      </List>
+    </Drawer>
+  );
+};
+
+export default MiniDrawer;
