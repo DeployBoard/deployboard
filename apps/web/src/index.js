209fc5a963697230de3401271f64cd8ee2a38852
Matt Karmazyn <matt@itskarma.wtf>
2022-09-03 09:30:40 -0400

    light/dark theme

--- apps/web/src/index.js
+++ apps/web/src/index.js
@@ -14,22 +14,32 @@ import "@fontsource/roboto/700.css";
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
+const lightOrDark = () => {
+  // check if the user has theme preference in localStorage
+  const themePreference = localStorage.getItem("theme");
+  if (themePreference) {
+    return themePreference;
+  }
+
+  // if not, check the system theme
+  if (
+    window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches
+  ) {
+    return "dark";
+  } else {
+    return "light";
+  }
+};
+
 const theme = createTheme({
   palette: {
-    mode: "light",
+    mode: lightOrDark(),
   },
   components: {
     MuiButtonBase: {
       defaultProps: {
         disableRipple: true,
-      },
-    },
-    MuiAppBar: {
-      styleOverrides: {
-        colorPrimary: {
-          backgroundColor: "white",
-          boxShadow: "none",
-        },
       },
     },
   },
