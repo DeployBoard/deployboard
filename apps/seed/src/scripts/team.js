1754d01f91f86f8e758678f13e4206ecd224da39
Matt Karmazyn <matt@itskarma.wtf>
2022-09-07 14:40:05 -0400

    meta and externalLinks

--- apps/seed/src/scripts/team.js
+++ apps/seed/src/scripts/team.js
@@ -2,17 +2,31 @@ const generateTeams = () => {
   let teams = [];
   teams.push({
     name: "Team 1",
-    description: "Team 1 description",
     account: "Seed",
     members: ["admin@seed.seed", "admin2@seed.seed"],
-    tags: ["tag1", "tag2"],
+    meta: {
+      Description: "This is team 1, they are awesome",
+    },
+    externalLinks: {
+      "Confluence Space": "https://confluence.com",
+      "Slack Channel": "https://slack.com",
+      "Jira Project": "https://jira.com",
+      "Github Team": "https://github.com",
+    },
   });
   teams.push({
     name: "Team 2",
-    description: "Team 2 description",
     account: "Seed",
     members: ["seedSaml@seedSaml.seed", "seedSaml2@seedSaml.seed"],
-    tags: ["tag2", "tag3"],
+    meta: {
+      Description: "This is team 2, they are also awesome",
+    },
+    externalLinks: {
+      "Confluence Space": "https://confluence.com",
+      "Slack Channel": "https://slack.com",
+      "Jira Project": "https://jira.com",
+      "Github Team": "https://github.com",
+    },
   });
   return teams;
 };
