619b0129008ee00ab534a10bc5f6a08eaf2a9ff0
Matt Karmazyn <matt@itskarma.wtf>
2025-12-21 21:18:29 -0500

    better error handling (#54)

--- apps/web/src/components/utils/axiosInterceptor.js
+++ apps/web/src/components/utils/axiosInterceptor.js
@@ -9,13 +9,18 @@ const setupAxiosInterceptor = () => {
       return response;
     },
     (error) => {
-      // If we get a 401 error, the token is expired or invalid
+      // If we get a 401 error, it means authentication failed (expired/invalid token)
       if (error.response && error.response.status === 401) {
         // Clear the tokens
         revokeTokens();
-        // Redirect to login with a loggedOut parameter
+        // Redirect to login with a sessionExpired parameter
         window.location.href = "/login?sessionExpired=true";
+        return Promise.reject(error);
       }
+      
+      // If we get a 403 error, it means authorization failed (valid token but insufficient permissions)
+      // Let the component handle it - don't log the user out
+      
       // For other errors, reject the promise so they can be handled by the component
       return Promise.reject(error);
     }
